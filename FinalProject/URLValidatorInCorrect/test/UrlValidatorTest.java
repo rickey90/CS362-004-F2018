55a2678c87f2657d62d64ac26cd98ede88eaa168
Rickey Patel <patelri@flip1.engr.oregonstate.edu>
2018-12-01 20:23:07 -0800

    Added all tests

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,4 +1,6 @@
 import junit.framework.TestCase;
+import java.util.Random;
+
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -458,5 +460,87 @@ public class UrlValidatorTest extends TestCase {
          //trueURL = urlVal.isValid("http://www.amazon.com");
          //System.out.printf(trueURL + "   http://www.amazon.com...this should be true...\n");     
    }//end of testIsValid
+
+   ResultPair[] testUrlScheme = {new ResultPair("http://", true),
+           new ResultPair("ftp://", true),
+           new ResultPair("h3t://", true),
+           new ResultPair("3ht://", false),
+           new ResultPair("http:/", false),
+           new ResultPair("http:", false),
+           new ResultPair("http/", false),
+           new ResultPair("://", false),
+           new ResultPair("", true)};
+
+   ResultPair[] testUrlAuthority = {new ResultPair("www.google.com", true),
+           new ResultPair("go.com", true),
+           new ResultPair("go.au", true),
+           new ResultPair("0.0.0.0", true),
+           new ResultPair("255.255.255.255", true),
+           new ResultPair("256.256.256.256", false),
+           new ResultPair("255.com", true),
+           new ResultPair("1.2.3.4.5", false),
+           new ResultPair("1.2.3.4.", false),
+           new ResultPair("1.2.3", false),
+           new ResultPair(".1.2.3.4", false),
+           new ResultPair("go.a", false),
+           new ResultPair("go.a1a", false),
+           new ResultPair("go.1aa", false),
+           new ResultPair("aaa.", false),
+           new ResultPair(".aaa", false),
+           new ResultPair("aaa", false),
+           new ResultPair("", false)
+   };
+   ResultPair[] testUrlPort = {new ResultPair(":80", true),
+         new ResultPair(":65535", true),
+         new ResultPair(":0", true),
+         new ResultPair("", true),
+         new ResultPair(":-1", false),
+         new ResultPair(":65636",false),
+         new ResultPair(":65a", false)
+   };
+   ResultPair[] testPath = {new ResultPair("/test1", true),
+         new ResultPair("/t123", true),
+         new ResultPair("/$23", true),
+         new ResultPair("/..", false),
+         new ResultPair("/../", false),
+         new ResultPair("/test1/", true),
+         new ResultPair("", true),
+         new ResultPair("/test1/file", true),
+         new ResultPair("/..//file", false),
+         new ResultPair("/test1//file", false)
+   };
+   //Test allow2slash, noFragment
+   ResultPair[] testUrlPathOptions = {new ResultPair("/test1", true),
+           new ResultPair("/t123", true),
+           new ResultPair("/$23", true),
+           new ResultPair("/..", false),
+           new ResultPair("/../", false),
+           new ResultPair("/test1/", true),
+           new ResultPair("/#", false),
+           new ResultPair("", true),
+           new ResultPair("/test1/file", true),
+           new ResultPair("/t123/file", true),
+           new ResultPair("/$23/file", true),
+           new ResultPair("/../file", false),
+           new ResultPair("/..//file", false),
+           new ResultPair("/test1//file", true),
+           new ResultPair("/#/file", false)
+   };
+
+   ResultPair[] testUrlQuery = {new ResultPair("?action=view", true),
+          new ResultPair("?action=edit&mode=up", true),
+          new ResultPair("", true)
+   };
+
+   Object[] testUrlParts = {testUrlScheme, testUrlAuthority, testUrlPort, testPath, testUrlQuery};//an array of arrays(which start on line 203
+   Object[] testUrlPartsOptions = {testUrlScheme, testUrlAuthority, testUrlPort, testUrlPathOptions, testUrlQuery};
+   int[] testPartsIndex = {0, 0, 0, 0, 0};
+
+   //---------------- Test data for individual url parts ----------------
+   ResultPair[] testScheme = {new ResultPair("http", true),
+        new ResultPair("ftp", false),
+        new ResultPair("httpd", false),
+        new ResultPair("telnet", false)
+   };
 
 }
